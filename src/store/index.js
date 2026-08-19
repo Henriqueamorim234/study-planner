77a77b41afd9ba667756463e597edbc01277a7fb
Henrique <henriqueamorim194@gmail.com>
2026-08-19 17:33:00 -0300

    feat: implement localStorage utilities for state management

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./slices/taskSlice";
 import themeReducer from "./slices/themeSlice";
 import analyticReducer from "./slices/analyticSlice";
+import { loadState, saveState, throttle } from "./localStorage";
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
@@ -9,4 +12,18 @@ export const store = configureStore({
     theme: themeReducer,
     analytics: analyticReducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
+  preloadedState,
+});
+
+const throttledSaveState = throttle((state) => {
+  saveState({
+    tasks: state.tasks,
+    theme: state.theme,
+    analytics: state.analytics,
+  });
+}, 1000);
+
+store.subscribe(() => {
+  throttledSaveState(store.getState());
 });
